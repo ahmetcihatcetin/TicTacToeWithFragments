c95828edc9cffbcd3414141f5cd249a83e2f7cd7
ahmetcihatcetin <ahmet.cetin@metu.edu.tr>
2022-11-24 17:23:02 +0300

    Button layouts have been finalised.

--- app/src/main/java/com/example/tictactoefragments/MainActivity.kt
+++ app/src/main/java/com/example/tictactoefragments/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.tictactoefragments
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.tictactoefragments.databinding.FragmentNavButtonsBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         val fragmentManager = supportFragmentManager.beginTransaction()
         fragmentManager.replace(R.id.placeHolderForNavButtons, NavButtons())
         fragmentManager.commit()
+
+
     }
 }
--- app/src/main/java/com/example/tictactoefragments/NavButtons.kt
+++ app/src/main/java/com/example/tictactoefragments/NavButtons.kt
@@ -1,10 +1,14 @@
 package com.example.tictactoefragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.example.tictactoefragments.databinding.FragmentNavButtonsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +20,16 @@ private const val ARG_PARAM2 = "param2"
  * Use the [NavButtons.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
+
 class NavButtons : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding: FragmentNavButtonsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +43,27 @@ class NavButtons : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_buttons, container, false)
+
+        _binding = FragmentNavButtonsBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        binding.buttonLeft.setOnClickListener(){
+            Toast.makeText(context,"Left button has been pressed",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonUp.setOnClickListener(){
+            Toast.makeText(context,"Up button has been pressed",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonRight.setOnClickListener(){
+            Toast.makeText(context,"Right button has been pressed",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonDown.setOnClickListener(){
+            Toast.makeText(context,"Down button has been pressed",Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 
     companion object {
